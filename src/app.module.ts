d505629d2666af10f5764e8abbcce1139f1c1012
l-qd <40837462+l-qd@users.noreply.github.com>
2022-09-28 20:57:07 +0400

    Added CRUD operations for auth, db, and storage

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './api/auth/auth.module';
+import { DbModule } from './api/db/db.module';
+import { StorageModule } from './api/storage/storage.module';
 
 // this is really important
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AuthModule, DbModule, StorageModule],
   controllers: [AppController],
   providers: [AppService],
 })
