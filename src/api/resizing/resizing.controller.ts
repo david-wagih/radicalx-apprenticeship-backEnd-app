8c36974f37dc835cfcf2046c851dfccb798977cd
Mariam Mohamed <mariammadbouly49@gmail.com>
2022-10-09 14:06:25 +0200

    error

--- src/api/resizing/resizing.controller.ts
+++ src/api/resizing/resizing.controller.ts
@@ -1,8 +1,45 @@
-import { Controller } from "@nestjs/common";
+import { Bind, Body, Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
 import { ResizingService } from "./resizing.service";
 
 
-@Controller()
+@Controller('uploads')
 export class ResizingController {
     constructor(private readonly resizingService: ResizingService) {}
+
+    @Get('upload')
+    getUploadedFiles(){
+        return this.resizingService.getUploadedFiles;
+    }
+
+    @Post('upload')
+    uploadFile(){
+        return this.resizingService.uploadFile;
+    }    
+
+    // @Post('upload')
+    // @UseInterceptors(FileInterceptor('file'))
+    // @Bind(UploadedFile())
+    // uploadFile(file) {
+    // console.log(file);
+    // }
+    // uploadFileAndPassValidation(
+    //   @UploadedFile(
+    //     new ParseFilePipeBuilder()
+    //       .addFileTypeValidator({
+    //         fileType: 'jpeg',
+    //       })
+    //       .addMaxSizeValidator({
+    //         maxSize: 1000
+    //       })
+    //       .build({
+    //         errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    //       }),
+    //   )
+    //   file: Express.Multer.File,
+    // ) {
+    //   return {
+    //     file: file.buffer.toString(),
+    //   };
+    // }
 }
--- src/api/resizing/resizing.service.ts
+++ src/api/resizing/resizing.service.ts
@@ -3,7 +3,36 @@ import { Injectable } from '@nestjs/common';
 // here we directly access data from database
 // here happens the heavy logic and magic
 
+const firebaseAdmin = require('firebase-admin');
+const { v4: uuidv4 } = require('uuid');
+
+const serviceAccount = require("./path/to/serviceAccountKey.json");
+
+const admin = firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+});
+
+const storageRef = admin.storage().bucket('gs://corei14-apprenticeship-app.appspot.com');
+
+
 @Injectable()
 export class ResizingService {
- 
+
+    getUploadedFiles() {
+        return 'This should be the uploaded files page';
+    }
+
+    async uploadFile(path, filename) {
+
+        const storage = await storageRef.upload(path, {
+            public: true,
+            destination: '/uploads/${filename}',
+            metadata: {
+                firebaseStorageDownloadTokens: uuidv4(),
+            }
+        });
+
+        return storage[0].metadata.mediaLink;
+    }
+    
 }
