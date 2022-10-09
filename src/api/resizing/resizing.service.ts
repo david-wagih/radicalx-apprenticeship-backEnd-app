babda618232c2f0b89ed1a862c2c800e60e01718
Mariam Mohamed <mariammadbouly49@gmail.com>
2022-10-10 00:10:15 +0200

    fixed error

--- src/api/resizing/resizing.service.ts
+++ src/api/resizing/resizing.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { ServiceAccount } from 'firebase-admin';
+import { AppModule } from 'src/app.module';
 
 // here we directly access data from database
 // here happens the heavy logic and magic
@@ -6,13 +10,7 @@ import { Injectable } from '@nestjs/common';
 const firebaseAdmin = require('firebase-admin');
 const { v4: uuidv4 } = require('uuid');
 
-const serviceAccount = require("./path/to/serviceAccountKey.json");
 
-const admin = firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
-});
-
-const storageRef = admin.storage().bucket('gs://corei14-apprenticeship-app.appspot.com');
 
 
 @Injectable()
@@ -22,7 +20,26 @@ export class ResizingService {
         return 'This should be the uploaded files page';
     }
 
-    async uploadFile(path, filename) {
+    async uploadFile(path: any, filename: any) {
+        const app = await NestFactory.create(AppModule);
+        app.useGlobalPipes(new ValidationPipe());
+        const configService: ConfigService = app.get(ConfigService);
+        const port: string = configService.get<string>('API_PORT');
+        const database_url: string = configService.get<string>('DB_URL');
+        const serviceAccount: ServiceAccount = {
+            projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+            privateKey: configService
+              .get<string>('FIREBASE_PRIVATE_KEY')
+              .replace(/\\n/g, '\n'),
+            clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+          };
+        
+        const admin = firebaseAdmin.initializeApp({
+            credential: firebaseAdmin.credential.cert(serviceAccount),
+            databaseURL: database_url,
+        });
+        
+        const storageRef = admin.storage().bucket('gs://corei14-apprenticeship-app.appspot.com');
 
         const storage = await storageRef.upload(path, {
             public: true,
@@ -34,5 +51,12 @@ export class ResizingService {
 
         return storage[0].metadata.mediaLink;
     }
+
+    //Test the upload function
+
+    // (async() => {
+    //     const url = await uploadFile('./mypic.png', "my-image.png");
+    //     console.log(url);
+    // })();
     
 }
