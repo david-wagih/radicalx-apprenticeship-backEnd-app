627549bc48746235887e74fb59044f553d936f87
l-qd <40837462+l-qd@users.noreply.github.com>
2022-10-17 00:51:31 +0400

    Started on uploading files to the storage

--- src/api/storage/storage.service.ts
+++ src/api/storage/storage.service.ts
@@ -1,11 +1,12 @@
 import { Body, Injectable, Param } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import {v4 as uuidv4} from 'uuid';
 @Injectable()
 export class StorageService {
-  uploadCompanyData(companyLogo: File, companyVideo: File) {
+  uploadCompanyData(apprenticeshipID: string, companyLogo: File, companyVideo: File) {
     //todo: Add the upload feature and return the references
     const bucket = admin.storage().bucket();
-        const uploadOptions = {
+        const imageOptions = {
             public: true,
             destination: "Apprenticeships/",
             gzip: true,
@@ -15,7 +16,18 @@ export class StorageService {
                 cacheControl: 'public, max-age:31536000',
             }
         };
+        const videoOptions = {
+          public: true,
+          destination: "Apprenticeships/",
+          gzip: true,
+          metadata: {
+              firebaseStorageDownloadTokens: uuidv4(),
+              contentType: 'video/mp4',
+              cacheControl: 'public, max-age:31536000',
+          }
+      };
 
+        bucket.upload(filePath, uploadOptions).then();
         const storage = await bucket.upload(filePath, uploadOptions);
         console.log(storage[0].metadata.mediaLink);
         return (storage[0].metadata.mediaLink);
