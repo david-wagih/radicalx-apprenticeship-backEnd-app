487e64242a003fc1ab2479ea5dd123126230c16f
Hazem Abdo <ha4450770@gmail.com>
2022-11-05 18:24:08 +0200

    working on integration

--- src/api/storage/storage.service.ts
+++ src/api/storage/storage.service.ts
@@ -3,43 +3,47 @@ import * as admin from 'firebase-admin';
 import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class StorageService {
-  async uploadCompanyData(
+  uploadCompanyData(
     apprenticeshipID: string,
     companyLogo: File,
     companyVideo: File,
   ) {
-    //todo: Add the upload feature and return the references
-    const bucket = admin.storage().bucket();
-    const imageOptions = {
-      public: true,
-      destination: 'Apprenticeships/' + apprenticeshipID,
-      gzip: true,
-      metadata: {
-        firebaseStorageDownloadTokens: uuidv4(),
-        contentType: 'image/png',
-        cacheControl: 'public, max-age:31536000',
-      },
-    };
-    const videoOptions = {
-      public: true,
-      destination: 'Apprenticeships/' + apprenticeshipID,
-      gzip: true,
-      metadata: {
-        firebaseStorageDownloadTokens: uuidv4(),
-        contentType: 'video/mp4',
-        cacheControl: 'public, max-age:31536000',
-      },
-    };
-    const logo = await bucket.upload(companyLogo.name, imageOptions);
-    const video = await bucket.upload(companyVideo.name, videoOptions);
-    console.log({
-      logo: 'Logo: ' + logo,
-      video: 'Video: ' + video,
-    });
     return {
-      logo: logo,
-      video: video,
+      video: 'video',
+      logo: 'logo',
     };
+    //todo: Add the upload feature and return the references
+    // const bucket = admin.storage().bucket();
+    // const imageOptions = {
+    //   public: true,
+    //   destination: 'Apprenticeships/' + apprenticeshipID,
+    //   gzip: true,
+    //   metadata: {
+    //     firebaseStorageDownloadTokens: uuidv4(),
+    //     contentType: 'image/png',
+    //     cacheControl: 'public, max-age:31536000',
+    //   },
+    // };
+    // const videoOptions = {
+    //   public: true,
+    //   destination: 'Apprenticeships/' + apprenticeshipID,
+    //   gzip: true,
+    //   metadata: {
+    //     firebaseStorageDownloadTokens: uuidv4(),
+    //     contentType: 'video/mp4',
+    //     cacheControl: 'public, max-age:31536000',
+    //   },
+    // };
+    // const logo = await bucket.upload(companyLogo.name, imageOptions);
+    // const video = await bucket.upload(companyVideo.name, videoOptions);
+    // console.log({
+    //   logo: 'Logo: ' + logo,
+    //   video: 'Video: ' + video,
+    // });
+    // return {
+    //   logo: logo,
+    //   video: video,
+    // };
   }
 
   updateCompanyData(
